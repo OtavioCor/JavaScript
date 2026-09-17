fbeff9436c3be9b6c00357c856c16b566ae4d9be
OtavioCor <otaviocorreiabr@gmail.com>
2026-09-17 16:57:49 -0300

    Aula BACKEND 17/09/2026

---
+++ Aula01/Exercicio01.js
@@ -0,0 +1,9 @@
+const qtdPecasPH = 120;
+const horasTurno = 8;
+
+const prodTotal = qtdPecasPH * horasTurno;
+
+console.log(`\nProducao por hora: ${qtdPecasPH}`);
+console.log(`Horas por turno: ${horasTurno}`);
+console.log(`Producao por turno: ${prodTotal}`);
+
